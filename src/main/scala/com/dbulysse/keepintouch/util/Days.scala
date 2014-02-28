ff2c39f1fa5c5a52d2b01c42e95f910b46bc43f8
Ben Pence <github@benpence.com>
2014-02-28 23:22:47 +0700

    Fixes big bug in inaccurate scheduling
    The scala compiler inferred the util.Days.MillisecondsPerDay constant was an
    Int instead of its correct type Long; thus, all operations involving it lacked
    precision and led to incorrect Dates.

--- src/main/scala/com/dbulysse/keepintouch/util/Days.scala
+++ src/main/scala/com/dbulysse/keepintouch/util/Days.scala
@@ -4,7 +4,7 @@ import java.util.Date
 
 object Days {
   val Today = new Date
-  val MillisecondsPerDay = 1000 * 60 * 60 * 24
+  val MillisecondsPerDay: Long = 1000 * 60 * 60 * 24
 
   def between(begin: Date, end: Date): Int =
     ((end.getTime - begin.getTime) / MillisecondsPerDay).toInt
